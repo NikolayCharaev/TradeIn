359c83f3d612f3f1e73d9012f30c8cdc1de782aa
Good_Cthulhu <hayyabusa_01@mail.ru>
2022-11-18 07:55:14 +0300

    project preparation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import CallNumbers from './components/callNumbers/CallNumbers';
+import './scss/style.scss'
+
 
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+      <>
+        <CallNumbers/>
+      </>
   );
 }
 
